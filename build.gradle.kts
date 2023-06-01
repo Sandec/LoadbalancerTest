b0850731a5200ec58988534be3462480df792e1e
Besmir Beqiri <bb@sandec.de>
2023-06-01 18:06:56 +0200

    Use Kotlin DSL for Gradle configuration

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("org.openjfx.javafxplugin") version "0.0.14"
+    `jpro-gradle-plugin`
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+repositories {
+    mavenCentral()
+}
+
+javafx {
+    version = "17.0.7"
+    modules = listOf("javafx.graphics", "javafx.controls", "javafx.fxml", "javafx.media", "javafx.web")
+}
+
+/**
+ * App Main Class
+ */
+application {
+    mainClass.set("com.jpro.loadbalancertest.LoadbalanderTestApp")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("fr.brouillard.oss:cssfx:11.5.1")
+}
+
+jpro {
+  // for debugging
+  jvmArgs = listOf("-Xmx100m")
+
+  //jpro server port
+  port = 8080
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+buildscript {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+
+        maven {
+            url = uri("https://sandec.jfrog.io/artifactory/repo")
+        }
+    }
+
+    dependencies {
+        classpath("one.jpro:jpro-gradle-plugin:2023.1.0")
+    }
+}
+
+rootProject.name = "LoadbalancerTest"
